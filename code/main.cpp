de7dc74cf33944593951999a88f7fb4b32d12b21
Chris-Abney-Carth <cabney@carthage.edu>
2025-11-03 12:57:15 -0600

    Added Leveling up
    Added leveling up, taking mostly from earthbound as the basis. Will modify with testing.

--- code/main.cpp
+++ code/main.cpp
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <cctype>
 #include <conio.h>
+#include <random>
 #include "item.h"
 #include "enemy.h"
 using namespace std;
@@ -38,7 +39,82 @@ int pMiscId = 0;
 vector<Item*> pItems;
 vector<Item*> iLookUp;
 vector<Enemy*> enemyList;
+int getRandom(int rMin, int rMax){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(rMin, rMax);
+    int randomValue = distrib(gen);
+    return randomValue;
+}
+int statBoost(int gRate, int level, int stat, int rand){
+    int statGain = ((gRate*level)-((stat-2)*10))*rand/50;
+}
 void saveFile();
+void levelUP(){
+    //stat growth rate
+    //atk = 10
+    //def = 6
+    //speed = 5
+    //agil = 5
+    //gut = 4
+    //wit = 3
+    cout << "LEVEL UP!" << endl;
+    int hpToAdd = pGuts*15;
+    maxHP += hpToAdd;
+    pHP += hpToAdd;
+    cout << "HP went up by " << hpToAdd << endl;
+    int mpToAdd = pWits*10;
+    maxMP += mpToAdd;
+    pMP = mpToAdd;
+    cout << "MP went up by " << mpToAdd << endl;
+    int min= 0;
+    int max = 0;
+    if ((pLevel+1)%4 == 0){
+        min = 7;
+        max = 10;
+    } else{
+        min = 3;
+        max = 10;
+    }
+    int atkRand = getRandom(min,max);
+    int attackBoost = statBoost(10, pLevel, pAttack, atkRand);
+    pAttack += attackBoost;
+    cout << "Attack went up by " << attackBoost << endl;
+
+    int defRand = getRandom(min,max);
+    int defenseBoost = statBoost(6, pLevel, pDefense, defRand);
+    pDefense += defenseBoost;
+    cout << "Defense went up by " << defenseBoost << endl;
+
+    int speRand = getRandom(min,max);
+    int speedBoost = statBoost(6, pLevel, pSpeed, speRand);
+    pSpeed += speedBoost;
+    cout << "Speed went up by " << speedBoost << endl;
+
+    int agiRand = getRandom(min,max);
+    int agilityBoost = statBoost(6, pLevel, pAgility, agiRand);
+    pAgility += agilityBoost;
+    cout << "Agility went up by " << agilityBoost << endl;
+
+    int gutRand = getRandom(min,max);
+    if ((pLevel+1) <= 10){
+        gutRand = 5;
+    }
+    int gutBoost = statBoost(6, pLevel, pGuts, gutRand);
+    pGuts += gutBoost;
+    cout << "Guts went up by " << gutBoost << endl;
+
+    int witRand = getRandom(min,max);
+    if ((pLevel+1) <= 10){
+        witRand = 5;
+    }
+    int witBoost = statBoost(6, pLevel, pWits, gutRand);
+    pWits += witBoost;
+    cout << "Wits went up by " << witBoost << endl;
+
+    pLevel += 1;
+    pExp = 0;
+}
 void setUpLookup(){
     //setup for lookUp Table
     int itemID = 000;
